535c7059f5d1fcd2f466c811eab7acd87c484db8
Harsh Rana <hr23232323@gmail.com>
2019-04-16 10:54:25 -0400

    bugs fixed

--- get_data.py
+++ get_data.py
@@ -2,28 +2,29 @@ import stock_scraper as ss
 import pandas as pd
 
 def createList():
-	df = pd.read_csv("sp500-constituents.csv")
-	stock_list = df['Symbol'].values
-	return stock_list
+	df_symbols = pd.read_csv("sp500-constituents.csv")
+	return df_symbols
 
 def main():
-	stock_list = createList()
+	df_symbols = createList()
 	interested = ['Market Cap (intraday)', 'Return on Equity', 'Revenue', 'Quarterly Revenue Growth', 
 	'Operating Cash Flow', 'Total Cash', 'Total Debt', 'Current Ratio', '52-Week Change',
 	'Avg Vol (3 month)', 'Avg Vol (10 day)', '% Held by Insiders']
 	technicals = {}
 	df = pd.DataFrame(columns=interested)
-	for each_stock in stock_list:
-		tech = ss.scrape_yahoo(each_stock)
+	for index, each_stock in df_symbols.iterrows():
+		tech = ss.scrape_yahoo(each_stock["Symbol"])
 		for ind in interested:	
 			try:
-				df.at[each_stock, ind] = tech[ind]
+				df.at[each_stock["Symbol"], ind] = tech[ind]
 			except Exception as e:
 				print('Failed, exception: ', str(e))
-		print("DONE- " + each_stock)
+		print(str(index+1) + ": DONE- " + each_stock["Symbol"])
+        
 	#Correct column name
+	df = df.reset_index()
 	df.rename(index=str, columns={df.columns[0]: "Symbol"}, inplace=True)
-	
+
 	# Merge symbols with data df to get name of company and industry
 	df = df.join(df_symbols.set_index('Symbol'), on="Symbol")
 
